c8ef21617ec299e518987ffa3f0d389b176b640f
Elliot Laurendeau <elliotb.laurendeau@bnc.ca>
2018-04-12 23:36:34 -0400

    adding javadoc

--- src/main/kotlin/extension/search/ArrayExtension.kt
+++ src/main/kotlin/extension/search/ArrayExtension.kt
@@ -1,3 +1,7 @@
 package extension.search
 
+/**
+ * Function used to know if an array is sorted
+ * @return true when sorted
+ */
 fun Array<Int>.isSorted(): Boolean = this.asSequence().let { it.zip(it.drop(1)).all { (a, b) -> a < b } }
